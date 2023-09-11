d37efe7006700ec78b1dc3c1794ecddf60445d10
Arisa Rae <Playarisa24@gmail.com>
2023-09-12 01:31:17 +0700

    submission 3

--- js/script.js
+++ js/script.js
@@ -1,33 +1,24 @@
-const inputField = document.getElementById("tinggi-input")
-const button = document.getElementById("totalArea")
-
-inputField.addEventListener('input', function() {
-    const inputValue = inputField.value
-
-    if (inputValidation(inputValue)) {
-        console.log("success")
-        button.disabled = false
-
-    } else {
-        console.log("fail")
-        button.disabled = true
+function luas() {
+    const alas = document.getElementById("inputAlas").value;
+    const tinggi = document.getElementById("inputTinggi").value;
+    const hasilLuas = document.getElementById("hasilLuas");
+    if (alas=="" || tinggi=="") {
+        hasilLuas.innerHTML = "Please enter valid number for both value.";
     }
-})
-
-
-document.getElementById(totalArea).addEventListener('click',function displayChange() {
-    console.log(this.value)
-    if (button:click)
+    else {
+        hasilLuas.innerHTML = "Luas Segitiga" + "<br>" + "= 1/2 x alas x tinggi" + "<br>" + "= 1/2 " + " x " + alas + " x " + tinggi + "<br>" + "= " + 1/2*alas*tinggi;
+    }
 }
 
-function inputValidation(input) {
-    const intergerRegex = /^-?\d+$/;
-    return intergerRegex.test(input)
-}
-
-function totalArea() {
-    let alas = document.getElementById("alas-input").value;
-    let tinggi = document.getElementById("tinggi-input").value;
-    let hasil = 1/2 * parseInt (alas) * parseInt (tinggi)
-    console.log("totalArea");
+function keliling() {
+    const sisiA = document.getElementById("inputA").value;
+    const sisiB = document.getElementById("inputB").value;
+    const sisiC = document.getElementById("inputC").value;
+    const hasilKeliling = document.getElementById("hasilKeliling");
+    if (sisiA=="" || sisiB=="" || sisiC=="") {
+        hasilKeliling.innerHTML = "Please enter valid number for both value.";
+    }
+    else {
+        hasilKeliling.innerHTML = "Keliling Segitiga" + "<br>" + "= sisi A + sisi B + sisi C" + "<br>" + "= " + sisiA + " + " + sisiB + " + " + sisiC + "<br>" + "= " + sisiA+sisiB+sisiC;
+    }
 }
